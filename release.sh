d46f5bc50f34d9b5de4b3edcec32b7df3963ad79
Jimmyshjj <jimmyshjj@gmail.com>
2022-06-22 21:04:59 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -118,9 +118,9 @@ function GenerateRules() {
                 # "https://dns.ipv6dns.com/dns-query"
                 # "https://doh.360.cn/dns-query"
                 # "https://doh.pub/dns-query"
-                # "tls://dns.alidns.com"
+                "tls://dns.alidns.com"
                 # "tls://dns.ipv6dns.com"
-                # "tls://dot.360.cn"
+                "tls://dot.360.cn"
                 "tls://dot.pub"
                 "tls://d.rubyfish.cn"
             )
